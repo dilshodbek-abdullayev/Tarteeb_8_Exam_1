6a0e1c703a8d3b8c13c4e7aa66aff8987ac6b291
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-06-13 10:03:42 +0500

    Variant-1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,41 @@
 ﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        int y = Console.WindowHeight / 2;
+        int x = Console.WindowWidth / 2;
+
+        ConsoleKeyInfo keyInfo;
+        Console.CursorVisible = false;
+
+        while(true){
+            Console.Clear();
+            Console.SetCursorPosition(x,y);
+            Console.Write("*");
+
+            if(Console.KeyAvailable){
+                keyInfo = Console.ReadKey(true);
+
+                switch(keyInfo.Key){
+                    case ConsoleKey.UpArrow:
+                        if(y > 0) y--;
+                            break;
+                    case ConsoleKey.RightArrow:
+                        if(x < Console.WindowWidth - 1) x++;
+                            break;
+                    case ConsoleKey.DownArrow:
+                        if(y < Console.WindowHeight - 1)y++;
+                            break;
+                    case ConsoleKey.LeftArrow:
+                        if(x > 0)x--;
+                            break;
+                }
+            }
+            Thread.Sleep(50);
+        }
+
+    }
+}
